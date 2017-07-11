151c6911e913f7802b7f96420dbd27b969726719
manas <s.manassahu@gmail.com>
2017-07-11 21:06:36 +0530

    Commiting chnages

---
+++ git_checkin.sh
@@ -0,0 +1,17 @@
+#/usr/bin/bash
+git init
+#git add --all #-->to add all files
+#git commit -m "first commit" #-->> commiting data
+#git push   --->Pushing daata to git repo
+git ls-files --modified | xargs git add #--> to add modified files 
+#git add -u
+git push
+
+#
+#
+#
+#
+#
+#
+#
+
--- test.c
+++ test.c
@@ -1,7 +1,20 @@
 #define STR1(X) #X
 #define STR(X) STR1(X)
-
 int main() {
   // prints correctly
   std::cout << STR(PROTO((int), f, (int)(a)(float)(b)));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
